d1547084c3a5b575fa13bdf79ddbc05e8e677085
Leoxzz <leooooxzz@gmail.com>
2025-02-14 14:14:13 -0500

    Updated content about recent life updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,10 +81,11 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
+    const categories = filterItems[i].dataset.category.toLowerCase().split(',').map(cat => cat.trim());
 
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category.toLowerCase()) {
+    } else if (categories.includes(selectedValue)) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
